77ab6ccc6081abee3ea57ddadaae0f39c617033a
svg onload=alert(1) <14846965+paradiseduo@users.noreply.github.com>
2025-02-18 18:59:19 +0800

    Update test-mac-inject.sh

--- test-mac-inject.sh
+++ test-mac-inject.sh
@@ -9,9 +9,12 @@ function build() {
 
 function testCase() {
   echo "==========Test Start=========="
-  ./inject inject -d @executable_path/testMac/libtestinject.dylib  
-  echo "==========Test Result=========="
+  ./inject inject -d @executable_path/testMac/libtestinject.dylib
+  codesign -s - -f --preserve-metadata=entitlements inject
+  echo "==========Test otool=========="
   otool -L inject
+  echo "==========Test Run=========="
+  ./inject
   echo "==========Test Finish=========="
 }
 
@@ -27,3 +30,34 @@ echo " "
 testCase
 echo " "
 clean
+
+function buildarm() {
+  echo "==========Build Start=========="
+  chmod +x build-macOS_arm.sh
+  ./build-macOS_arm.sh
+  echo "==========Build Finish=========="
+}
+
+function testCasearm() {
+  echo "==========Test Start=========="
+  ./inject_arm64 inject_arm64 -d @executable_path/testMac/libtestinject.dylib
+  codesign -s - -f --preserve-metadata=entitlements inject_arm64
+  echo "==========Test otool=========="
+  otool -L inject_arm64
+  echo "==========Test Run=========="
+  ./inject_arm64
+  echo "==========Test Finish=========="
+}
+
+function cleanarm() {
+  echo "==========Clean Start=========="
+  rm -rf inject_arm64
+  mv inject_arm64_back inject_arm64
+  echo "==========Clean Finish=========="
+}
+
+buildarm
+echo " "
+testCasearm
+echo " "
+cleanarm
